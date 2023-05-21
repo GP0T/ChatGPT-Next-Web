ab354739e846232c39106254f2ee8ca298891c0e
GP0T <134146415+GP0T@users.noreply.github.com>
2023-05-21 09:31:21 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT</div>
         <div className={styles["sidebar-sub-title"]}>
-          构建自己的AI助手。
+          用人工智能改变工作和创造力
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
